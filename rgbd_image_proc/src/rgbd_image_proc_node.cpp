4c280851c73401b0e520e48624add32ad6f53e5b
Ivan Dryanovski <ivan.dryanovski@gmail.com>
2012-12-04 11:52:41 -0500

    adding rgbd_image_proc package

---
+++ rgbd_image_proc/src/rgbd_image_proc_node.cpp
@@ -0,0 +1,11 @@
+#include "rgbd_image_proc/rgbd_image_proc.h"
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "RGBDImageProc");  
+  ros::NodeHandle nh;
+  ros::NodeHandle nh_private("~");
+  ccny_rgbd::RGBDImageProc rgbd_image_proc(nh, nh_private);
+  ros::spin();
+  return 0;
+}
